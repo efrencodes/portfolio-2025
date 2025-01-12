bf2a762c22431629b74016561500afdaba8749d8
efrencodes <efrencodes@gmail.com>
2025-01-12 01:02:29 -0800

    feat: Refactor ListItem component to accept props and create separate configurations for projects and posts

---
+++ src/config/projects.ts
@@ -0,0 +1,17 @@
+export interface ListProjectProps {
+  name: string;
+  description: string;
+  url: string;
+  alt: string;
+  urlImage: string;
+}
+
+export const ListProject: ListProjectProps[] = [
+  {
+    name: "calculadora de isr - occ mundial",
+    description: "Calculadora de ISR para empleados de OCC Mundial",
+    url: "https://www.occ.com.mx/salarios/calculadora-de-isr/",
+    alt: "Calculadora de ISR para empleados de OCC Mundial",
+    urlImage: "https://placehold.co/200x350"
+  },
+];
