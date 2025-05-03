8141d08a2bb67aee3146ab8d6f0ae51f8d5c8a51
efrencodes <efrencodes@gmail.com>
2025-05-02 23:53:48 -0700

    refactor: Update project card component and configuration for improved structure and data handling

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -1,18 +1,50 @@
-export interface ListProjectProps {
-	name: string
+export interface ListsProjectProps {
+	year: number
+	title: string
 	description: string
-	url: string
-	alt: string
-	urlImage: string
+	stack: string[]
+	images: string[]
 }
 
-export const ListProject: ListProjectProps[] = [
+export const ListsProject:ListsProjectProps[] = [
 	{
-		name: 'Calculadora de ISR - OCC Mundial',
+		year: 2024,
+		title: 'Nuevo flujo de creación de cuenta at OCC mundial ↗',
+		description:
+			'Se mejoró el flujo para la creación de cuentas de nuevos candidatos con un enfoque especial en dispositivos móviles. Ahora, se evitará el abandono durante el flujo, asegurando que los CV sean completos y precisos.',
+		stack: [
+			'React + Next14 con los features de React Server Components y Client Server Components.',
+			'Tailwindcss',
+			'Apollo Server y GraphQL',
+			'Pruebas unitarias con Jest',
+			'Pruebas automatizadas con Cypress',
+		],
+		images: [
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+		],
+	},
+	{
+		year: 2024,
+		title: 'Calculadora de ISR',
 		description:
 			'Conoce tu ingreso real después de retenciones y enfoca tu búsqueda en empleos que cumplan tus metas.',
-		url: '/proyectos/calculadora-de-isr',
-		alt: '', // "Calculadora de ISR para empleados de OCC Mundial",
-		urlImage: '', // "/proyectos/calculadora-de-isr/home.png"
+		stack: [
+			'React + Next14 con los features de React Server Components y Client Server Components.',
+			'Tailwindcss',
+			'Apollo Server y GraphQL',
+			'Pruebas unitarias con Jest',
+			'Pruebas automatizadas con Cypress',
+		],
+		images: [
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+			'/placeholder.svg?height=150&width=80',
+		],
 	},
 ]
