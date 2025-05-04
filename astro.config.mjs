a98fda3dd3c29a111918b20f29868504abcb0f20
efrencodes <efrencodes@gmail.com>
2025-05-03 23:05:36 -0700

    feat: Add Vercel adapter with web analytics configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
