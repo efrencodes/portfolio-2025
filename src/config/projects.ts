0cb53f8afa744a84531a4d6159cc8ea27dc9bf61
efrencodes <efrencodes@gmail.com>
2025-05-12 22:25:05 -0700

    feat: Add href properties to project entries and update image references in projects configuration

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -2,6 +2,7 @@ export interface ListsProjectProps {
 	year: number
 	title: string
 	description: string
+	href: string
 	stack: string[]
 	images: string[]
 }
@@ -10,25 +11,9 @@ export const ListsProject: ListsProjectProps[] = [
 	{
 		year: 2024,
 		title: 'Nuevo flujo de creación de cuenta | OCC mundial',
+		href: 'https://www.occ.com.mx/candidatos/cuenta-nueva/registro/',
 		description:
 			'Se mejoró el flujo para la creación de cuentas de nuevos candidatos con un enfoque especial en dispositivos móviles. Ahora, se evitará el abandono durante el flujo, asegurando que los CV sean completos y precisos.',
-		stack: [
-			'React + Next14 con los features de React Server Components y Client Server Components.',
-			'Tailwindcss',
-			'Apollo Server y GraphQL',
-			'Pruebas unitarias con Jest',
-			'Pruebas automatizadas con Cypress',
-		],
-		images: [
-			'/proyectos/calculadora-de-isr/page-calculator-grafic.png',
-			'/proyectos/calculadora-de-isr/page-sueldo-neto.png',
-		],
-	},
-	{
-		year: 2024,
-		title: 'Calculadora de ISR | OCC Mundial',
-		description:
-			'Conoce tu ingreso real después de retenciones y enfoca tu búsqueda en empleos que cumplan tus metas.',
 		stack: [
 			'React + Next14 con los features de React Server Components y Client Server Components.',
 			'Tailwindcss',
@@ -42,5 +27,23 @@ export const ListsProject: ListsProjectProps[] = [
 			'/proyectos/cvmatic/page-sube-tu-cv-initial.png',
 			'/proyectos/cvmatic/page-sube-tu-cv.png',
 		],
+	},
+	{
+		year: 2024,
+		title: 'Calculadora de ISR | OCC Mundial',
+		href: 'https://www.occ.com.mx/salarios/calculadora-de-isr/',
+		description:
+			'Conoce tu ingreso real después de retenciones y enfoca tu búsqueda en empleos que cumplan tus metas.',
+		stack: [
+			'React + Next14 con los features de React Server Components y Client Server Components.',
+			'Tailwindcss',
+			'Apollo Server y GraphQL',
+			'Pruebas unitarias con Jest',
+			'Pruebas automatizadas con Cypress',
+		],
+		images: [
+			'/proyectos/calculadora-de-isr/page-calculator-grafic.png',
+			'/proyectos/calculadora-de-isr/page-sueldo-neto.png',
+		]
 	},
 ]
